29abf2968d22ab1a0d50275044e7226eabbd425c
Evgeny Vyazovtsev <evo118@outlook.com>
2020-02-04 22:20:54 +0300

    Initial commit

---
+++ MultiSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MultiSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
